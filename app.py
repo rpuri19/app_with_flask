b5dd9bfba0fda10f9632c5836fe78bf55953c18c
rpuri19 <ruchipuri19@gmail.com>
2025-02-03 00:31:10 +0100

    updated index.html

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
+@app.route('/')
+def index():
+    with open('data/data.json') as file:
+        blog_posts = json.load(file)
+    return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
